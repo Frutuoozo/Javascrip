fea0b23ffc53ad37b0ddae7772b0387cdb8e86fc
Frutuoozo <leticiafrutuozo681@gmail.com>
2024-03-20 00:12:10 -0300

    novo des
    cursoemvideo

---
+++ Execício/ex003/Repetições.js
@@ -0,0 +1,22 @@
+//Repetições em JS
+
+
+
+
+/*Não importa a quantidade de repetições
+    while é a msm coisa de "enquanto"
+        serve como forma de teste seja no inicio ou final*/
+var c = 1 
+while (c <= 10) [
+    console.log(`passo ${c}`),
+    c++
+]
+
+var c = 1 
+do[ console.log(`ex ${c}`),
+    c++
+]; while (c <= 10)
+
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
